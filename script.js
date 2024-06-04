dc8ee12dcd189030249cff58fa50c69d99665719
gauravsahdz <sahgaurav33@gmail.com>
2024-06-04 21:03:23 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function submitForm(event) {
+  event.preventDefault();
+  const phone = document.getElementById("phone").value;
+  const query = document.getElementById("query").value;
+  if (phone === "" || query === "") {
+    alert("Please fill all the fields!");
+    return;
+  }
+  if (phone.length !== 10) {
+    alert("Please enter a valid phone number!");
+    return;
+  }
+
+  document.getElementById("phone").value = "";
+  document.getElementById("query").value = "";
+  alert("Your query has been submitted successfully!");
+}
